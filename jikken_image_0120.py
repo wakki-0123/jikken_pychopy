47d8ce3ab46b1e49d40a1b3f76628fc328205241
wakita <152936883+wakki-0123@users.noreply.github.com>
2024-01-20 11:25:16 +0900

    0120_final

--- jikken_image_0120.py
+++ jikken_image_0120.py
@@ -53,12 +53,17 @@ def click2(position,position1,position2):
 # 画像検索関数
 def psychopy00():
     cwd = os.getcwd()  # 現在の作業ディレクトリ
-    imageLists = glob.glob(cwd + "/jikken_0120_1/*.jpg") # 任意のファイルがあるディレクトリ
+    imageLists = glob.glob(cwd + "/jikken_0120_3/*.jpg") # 任意のファイルがあるディレクトリ
     return imageLists
 
 def psychopy11():
     cwd = os.getcwd()  # 現在の作業ディレクトリ
-    imageLists = glob.glob(cwd + "/jikken_0120_2/*.jpg") # 任意のファイルがあるディレクトリ
+    imageLists = glob.glob(cwd + "/jikken_0120_4/*.jpg") # 任意のファイルがあるディレクトリ
+    return imageLists
+
+def psychopy22():
+    cwd = os.getcwd()  # 現在の作業ディレクトリ
+    imageLists = glob.glob(cwd + "/OIP.jpg") # 任意のファイルがあるディレクトリ
     return imageLists
 
 # 画像読み込み関数
@@ -68,31 +73,73 @@ def psychopy0(imageLists):
         imageData[i] = visual.ImageStim(win, image=i)
     return imageData
 
-def psychopy1(imageLists):
+def psychopy01(imageLists):
     imageData = {}
     for i in imageLists:
         imageData[i] = visual.ImageStim(win, image=i)
     return imageData
 
 
+def psychopy2(imageLists):
+    imageData = {}
+    for i in imageLists:
+        imageData[i] = visual.ImageStim(win, image=i)
+    return imageData
+
 # 画像表示関数
-def psychopy(imageLists, imageData, time3):
+def psychopy(imageLists, imageData, time3,imageData2,j):
     print('画像提示開始時刻:',time3)
     time4 = time.perf_counter()
     write_to_csv(time3)
-    j = 0
+    
+    
+
 
     for i in imageLists:
         j = j + 1
        
         imageData[i].draw()
         win.flip()
-        core.wait(10) # 画像の表示時間　(この書き方だと，基本的にすべての画像は10秒間表示される)
+        core.wait(3) # 画像の表示時間　(この書き方だと，基本的にすべての画像は10秒間表示される)
         time5 = time.perf_counter()
         time6 = (time5 - time4) + time3
         print('画像提示終了時刻その',j)  # 画像が終わった時間
         print(time6)
         write_to_csv(time6)
+    # imageData2を辞書として利用
+        for key in imageData2:
+            imageData2[key].draw()
+            win.flip()
+            core.wait(2)
+            time5 = time.perf_counter()
+            time6 = (time5 - time4) + time3
+            print('短いインターバル')
+            print(time6)
+            write_to_csv(time6)
+
+        
+    a = 1
+    return a,time6,j
+
+def psychopy1(imageLists, imageData, time3):
+    print('画像提示開始時刻:',time3)
+    time4 = time.perf_counter()
+    write_to_csv(time3)
+    k = 0
+
+    for i in imageLists:
+        k = k + 1
+       
+        imageData[i].draw()
+        win.flip()
+        core.wait(10) # 画像の表示時間　(この書き方だと，基本的にすべての画像は10秒間表示される)
+        time5 = time.perf_counter()
+        time6 = (time5 - time4) + time3
+        print('長いインターバル')  # 画像が終わった時間
+        print(time6)
+        write_to_csv(time6)
+    a = 2
+    return a,time6
 
 def write_to_csv(time_value):
     # ファイルは新規作成したものを入れよう(そのまま上書きされるので)  EX) time_log1.csv
@@ -128,10 +175,15 @@ if __name__ == "__main__":
     win = visual.Window(size=(1919,1076), pos=(0,0), screen=1) # size: 画面の大きさ(信川Labのモニターの大きさと同じにしている) pos: 画面の座標 screen = ( 0 OR 1 OR 2 ) 画像を表示させるスクリーンの指定
     imageLists = psychopy00()
     imageLists1 = psychopy11()
+    imageLists2 = psychopy22()
     imageData = psychopy0(imageLists)
-    imageData1 = psychopy1(imageLists1)
+    imageData1 = psychopy01(imageLists1)
+    imageData2 = psychopy2(imageLists2)
+
 
     # 画像，経過時間の表示　また，画像提示時刻をcsvファイルに記録する
+    a = 0
+    b = 0
     i = 0
     while True:
         i += 1
@@ -144,8 +196,14 @@ if __name__ == "__main__":
         time2 = time.perf_counter() # 1秒ごとに時間を取得
         print("経過時間:", time2 - time1) # 1秒ごとに経過時間を表示
         time3 = time2 - time1
-        if int(time3) == 30: # クリックしてから30秒後に画像提示をするようになっている
-            psychopy(imageLists, imageData, time3)
+        if int(time3) == 10: # クリックしてから30秒後に画像提示をするようになっている
+            [a,time6,j] = psychopy(imageLists, imageData, time3,imageData2,0)
 
-        if int(time3) == 240: # クリックしてから240秒後に画像提示をするようになっている
-            psychopy(imageLists1, imageData1, time3)
+        if a == 1:
+            [b,time7] = psychopy1(imageLists2, imageData2, time6)
+
+        if b == 2: 
+            [c,time8,k] = psychopy(imageLists1, imageData1,time7,imageData2,j)
+        a = 0
+        b = 0
+        
